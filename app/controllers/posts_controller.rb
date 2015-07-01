1376f715f4c84f5375ce668428e6ae8c254aab24
Rui Pereira <ruijosemangas@gmail.com>
2015-07-01 22:47:32 +0100

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.create
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def post_params  
+    params.require(:post).permit(:image, :caption)
+  end  
+end
