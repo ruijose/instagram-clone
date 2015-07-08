435cdf8e71ca7b81041c56e4dd25913bc7e7270c
Rui Pereira <ruijosemangas@gmail.com>
2015-07-08 14:39:42 +0100

    removed ajax form app

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,11 +2,17 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:like][:liked_pics_id])
     current_user.like(@post)
-    redirect_to root_path
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js
+    end
   end
 
   def destroy
     like = Like.find(params[:id]).destroy
-    redirect_to root_path
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js
+    end
   end
 end
