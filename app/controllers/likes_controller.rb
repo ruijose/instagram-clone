d94cd8af129f158204a045526859b58acdcb5639
Rui Pereira <ruijosemangas@gmail.com>
2015-07-15 14:49:14 +0100

    ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,19 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:like][:liked_pics_id])
     current_user.like(@post)
-    redirect_to root_path
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { render :file => "likes/toggle.js.erb" }
+    end
   end
 
   def destroy
     like = Like.find(params[:id])
     @post = Post.find(like.liked_pics_id) 
     like.destroy
-    redirect_to root_path
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { render :file => "likes/toggle.js.erb" }
+    end
   end
 end
