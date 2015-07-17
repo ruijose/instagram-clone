4e8a000a57b7d73f9941af30a46de9e12ba383ec
Rui Pereira <ruijosemangas@gmail.com>
2015-07-17 10:36:49 +0100

    following users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,26 @@ class User < ActiveRecord::Base
   has_many :likes,      :foreign_key => "who_liked_id"
   has_many :comments,   :dependent => :destroy
   has_many :liked_pics, through: :likes
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
 
   def like(post)
     self.likes.create(:liked_pics_id => post.id)
+  end
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714121129) do
+ActiveRecord::Schema.define(version: 20150717091545) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "body"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 20150714121129) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
