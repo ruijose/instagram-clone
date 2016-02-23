f034253b98b248cc17541e72db22dcb98f8a16e4
ruijose <ruijosemangas@gmail.com>
2016-02-23 14:41:40 +0000

    more specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,45 @@
 require 'rails_helper'
 
 describe Post do
-  it 'has a valid factory' do
-    expect(create(:post)).to be_valid
+  let(:lisa)  { create(:user, email: "lisa@simpsons.com") }
+  let(:bart)  { create(:user, email: "bart@simpsons.com") }
+  let(:magie) { create(:user, email: "magie@simpsons.com") }
+  let(:post)  { create(:post) }
+
+  describe 'post creation validation' do
+    it 'has a valid factory' do
+      expect(lisa).to be_valid
+      expect(post).to be_valid
+    end
+
+    it 'is invalid without an image' do
+      expect(build(:post, image: nil)).to_not be_valid
+    end
+
+    it 'is invalid without a caption' do
+      expect(build(:post, caption: nil)).to_not be_valid
+    end
+  end
+
+  describe "#followed_users_posts" do
+    before :each do
+      first_post   = create(:post, user_id: lisa.id)
+      second_post  = create(:post, user_id: magie.id)
+      third_post   = create(:post, user_id: magie.id)
+      bart.follow!(magie)
+      magie.follow!(lisa)
+    end
+
+    it "returns 2 posts, bart only follows magie" do
+      expect(Post.followed_users_posts(bart).size).to eq(2)
+    end
+
+    it "returns 1 posts, magie follows lisa" do
+      expect(Post.followed_users_posts(magie).size).to eq(1)
+    end
+
+    it "returns 0 posts, lisa follow noone" do
+      expect(Post.followed_users_posts(lisa).size).to eq(0)
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,39 +1,77 @@
 require 'rails_helper'
 
 describe User do
-  before :each do
-    @lisa = create(:user, email: "lisa@simpsons.com")
-    @bart = create(:user, email: "bart@simpsons.com")
-    @post = create(:post)
+  let(:lisa) { create(:user, email: "lisa@simpsons.com") }
+  let(:bart) { create(:user, email: "bart@simpsons.com") }
+  let(:post) { create(:post) }
+
+  describe 'user attributed validations' do
+    it 'has a valid factory' do
+      expect(create(:user, email: "magie@simpsons.com")).to be_valid
+    end
+
+    it 'is invalid without an email' do
+      expect(build(:user, email: nil)).to_not be_valid
+    end
+
+    it 'is invalid without a password' do
+      expect(build(:user, password: nil)).to_not be_valid
+    end
+
+    it 'is invalid with a wrong email format' do
+      expect(build(:user, email: "jj.pt")).to_not be_valid
+    end
+
+    it 'is invalid with duplicate email address' do
+      lisa_one = lisa
+      lisa_two = build(:user, email: "lisa@simpsons.com")
+      expect(lisa_two).to_not be_valid
+    end
   end
 
-  it 'has a valid factory' do
-    expect(create(:user)).to be_valid
+  describe '#username' do
+    it 'returns a username' do
+      expect(lisa.username).to eq("lisa")
+    end
+
+    it 'returns a username' do
+      expect(bart.username).to eq("bart")
+    end
   end
 
-  it 'is invalid without an email' do
-    expect(build(:user, email: nil)).not_to be_valid
+  describe '#like' do
+    it 'allows a user to like a post' do
+      bart.like(post)
+      expect(bart.liked_pics.count).to eq(1)
+    end
+
+    it 'raises an error' do
+      expect { bart.like(nil) }.to raise_error(NoMethodError)
+    end
   end
 
-  it 'is invalid if passwords does not match' do
-    expect(build(:user, password: "asdasd", password_confirmation: "12123")).not_to be_valid
+  describe '#follow' do
+    it 'allows a user to follow another users' do
+      bart.follow!(lisa)
+      expect(bart.following?(lisa)).to eq(true)
+      expect(bart.followed_users.count).to eq(1)
+    end
+
+    it 'raises an error' do
+      expect { bart.follow!(nil) }.to raise_error(NoMethodError)
+    end
   end
 
-  it 'returns a username' do
-    expect(@lisa.username).to eq("lisa")
-  end
+  describe '#unfollow' do
+    it 'allow a user to unfollow another users' do
+      bart.follow!(lisa)
+      bart.unfollow!(lisa)
+      expect(bart.following?(lisa)).to eq(false)
+      expect(bart.followed_users.count).to eq(0)
+    end
 
-  it 'allows a user to like a post' do
-    @bart.like(@post)
-    expect(@bart.liked_pics.count).to eq(1)
-  end
-
-  it 'allows a user to follow and unfollow another users' do
-    @bart.follow!(@lisa)
-    expect(@bart.following?(@lisa)).to eq(true)
-    expect(@bart.followed_users.count).to eq(1)
-    @bart.unfollow!(@lisa)
-    expect(@bart.following?(@lisa)).to eq(false)
-    expect(@bart.followed_users.count).to eq(0)
+    it 'raises an error' do
+      expect { bart.unfollow!(nil) }.to raise_error(NoMethodError)
+    end
   end
 end
